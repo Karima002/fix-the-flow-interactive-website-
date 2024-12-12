c43614d99ea8faeb003ae73a24a5d3c746505567
Karima002 <karima.mouadi@hva.nl>
2024-12-12 23:39:46 +0100

    search gefixt

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 // stap 1: zoek de menu-button op en sla die op in een variabele
 let openButton = document.querySelector('header > .button--menu-open'); //hiermee wordt de de button menu opgezocht. //In JavaScript wordt const gebruikt om een variabele te declareren die niet hernoemd of opnieuw toegewezen kan worden.
-let deNav = document.querySelector('nav');
+let NavSearch = document.querySelector('nav');
 let sluitButton = document.querySelector('header > .button--menu-sluit');  //sluitbutton in de navigatiebar wordt opgezocht
 
 // stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
@@ -9,14 +9,14 @@ openButton.addEventListener('click', openMenu); // Zo ga je een event (actie) to
 
 // stap 3: voeg in de functie een class toe aan de nav
 function openMenu (){
-    deNav.classList.toggle('toonMenu');
+    NavSearch.classList.toggle('toonMenu');
 }
 
-sluitButton.addEventListener('click', sluitMenu);
+// sluitButton.addEventListener('click', sluitMenu);
 
 // stap 4 - in de functie verwijder de class van de nav
 function sluitMenu() {
-    deNav.classList.remove('toonMenu');
+    NavSearch.classList.remove('toonMenu');
 }
 
 
@@ -26,13 +26,14 @@ const openSearchButton = document.querySelector('.search-button');
 const theSearch = document.querySelector('.zoeken');
 
 //Stap 2: voeg event en functie toe
-openSearchButton.addEventListener('click', openSearch);
 
+openSearchButton.addEventListener('click', openSearch);
 function openSearch(){
     theSearch.classList.add('toonSearch');
 }
-
 //Stap 3: feedback aan de gebruiker --> CSS
+
+
 
 
 /* experiment met andere javascript voor menubalk
