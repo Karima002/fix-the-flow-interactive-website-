a4f614ba9209f736641aa1111058946ec3aac905
Karima002 <karima.mouadi@hva.nl>
2024-12-12 22:54:44 +0100

    Interactie met search button gemaakt #19

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 // stap 1: zoek de menu-button op en sla die op in een variabele
-let openButton = document.querySelector('header > button--menu-open'); //hiermee wordt de de button menu opgezocht. //In JavaScript wordt const gebruikt om een variabele te declareren die niet hernoemd of opnieuw toegewezen kan worden.
+let openButton = document.querySelector('header > .button--menu-open'); //hiermee wordt de de button menu opgezocht. //In JavaScript wordt const gebruikt om een variabele te declareren die niet hernoemd of opnieuw toegewezen kan worden.
 let deNav = document.querySelector('nav');
-let sluitButton = document.querySelector('header > button--menu-sluit');  //sluitbutton in de navigatiebar wordt opgezocht
+let sluitButton = document.querySelector('header > .button--menu-sluit');  //sluitbutton in de navigatiebar wordt opgezocht
 
 // stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
 openButton.addEventListener('click', openMenu); // Zo ga je een event (actie) toevoegen. We moeten nog wel zeggen wat de click gaat doen:
@@ -17,6 +17,17 @@ sluitButton.addEventListener('click', sluitMenu);
 // stap 4 - in de functie verwijder de class van de nav
 function sluitMenu() {
     deNav.classList.remove('toonMenu');
+}
+
+
+//MARK: SEARCH
+const openSearchButton = document.querySelector('.search-button');
+const theSearch = document.querySelector('.zoeken');
+
+openSearchButton.addEventListener('click', openSearch);
+
+function openSearch(){
+    theSearch.classList.add('toonSearch');
 }
 
 
