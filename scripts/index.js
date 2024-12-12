d3dbef14514dee518340fb24ad8e5ede13761463
Karima002 <karima.mouadi@hva.nl>
2024-12-12 23:04:13 +0100

    added more comments

--- scripts/index.js
+++ scripts/index.js
@@ -21,14 +21,18 @@ function sluitMenu() {
 
 
 //MARK: SEARCH
+//Stap 1: selecteren geschikt element
 const openSearchButton = document.querySelector('.search-button');
 const theSearch = document.querySelector('.zoeken');
 
+//Stap 2: voeg event en functie toe
 openSearchButton.addEventListener('click', openSearch);
 
 function openSearch(){
     theSearch.classList.add('toonSearch');
 }
+
+//Stap 3: feedback aan de gebruiker --> CSS
 
 
 /* experiment met andere javascript voor menubalk
