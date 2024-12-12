6b93308cef4bdf9ca46acb96812f1dbbae1c58a9
Karima002 <karima.mouadi@hva.nl>
2024-12-12 15:27:26 +0100

    voorpagina nieuws artikel toegevoegd

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,34 @@
 // stap 1: zoek de menu-button op en sla die op in een variabele
-const openButton = //In JavaScript wordt const gebruikt om een variabele te declareren die niet hernoemd of opnieuw toegewezen kan worden.
-document.querySelector("header section button"); //hiermee wordt de de button menu opgezocht.
+let openButton = document.querySelector('header > button--menu-open'); //hiermee wordt de de button menu opgezocht. //In JavaScript wordt const gebruikt om een variabele te declareren die niet hernoemd of opnieuw toegewezen kan worden.
+let deNav = document.querySelector('nav');
+let sluitButton = document.querySelector('header > button--menu-sluit');  //sluitbutton in de navigatiebar wordt opgezocht
 
 // stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
-openButton.addEventListener("click", openMenu); // Zo ga je een event (actie) toevoegen. We moeten nog wel zeggen wat de click gaat doen:
+openButton.addEventListener('click', openMenu); // Zo ga je een event (actie) toevoegen. We moeten nog wel zeggen wat de click gaat doen:
+
 
 // stap 3: voeg in de functie een class toe aan de nav
 function openMenu (){
-    const deNav =
-    document.querySelector("nav");
-    deNav.classList.add("toonMenu");
+    deNav.classList.toggle('toonMenu');
 }
 
-// stap 4 - zoek sluit button op
-const sluitButton =
-document.querySelector("nav > button") //button in de navigatiebar wordt opgezocht
+sluitButton.addEventListener('click', sluitMenu);
 
-// stap 5: laat de menu-button luisteren naar kliks en voer dan een functie uit
-sluitButton.addEventListener("click", sluitMenu);
-
-// stap 6 - in de functie verwijder de class van de nav
+// stap 4 - in de functie verwijder de class van de nav
 function sluitMenu() {
-    const deNav =
-    document.querySelector("nav"); //Zoek de nav op en sla die op in een variable
-    deNav.classList.remove("toonMenu");
+    deNav.classList.remove('toonMenu');
 }
+
+
+/*
+//Stap 1: selecteren geschikt element
+let openButton = document.querySelector('header button--menu-open');
+let naviGation = document.querySelector('nav');
+
+//Stap 2: voeg event en functie toe
+openButton.addEventListener('click', openMenu);
+
+function openMenu() {
+    naviGation.classList.add('toonMenu');
+}
+*/
