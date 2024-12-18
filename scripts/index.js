03a95648ad76ef461c343e3d1d8128a6c5e5de56
Karima002 <karima.mouadi@hva.nl>
2024-12-18 21:36:50 +0100

    zoek & terug button gemaakt voor feedforward na gebruik zoekfunctie

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,42 @@
+// MARK: MENU
 // stap 1: zoek de menu-button op en sla die op in een variabele
-let openButton = document.querySelector('header > .button--menu-open'); //hiermee wordt de de button menu opgezocht. //In JavaScript wordt const gebruikt om een variabele te declareren die niet hernoemd of opnieuw toegewezen kan worden.
-let NavSearch = document.querySelector('nav');
-let sluitButton = document.querySelector('header > .button--menu-sluit');  //sluitbutton in de navigatiebar wordt opgezocht
+let openButton = document.querySelector('header > .button--menu-open') //hiermee wordt de de button menu opgezocht. //In JavaScript wordt const gebruikt om een variabele te declareren die niet hernoemd of opnieuw toegewezen kan worden.
+let NavSearch = document.querySelector('nav')
+let sluitButton = document.querySelector('header nav .button--menu-sluit')  //sluitbutton in de navigatiebar wordt opgezocht
 
 // stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
-openButton.addEventListener('click', openMenu); // Zo ga je een event (actie) toevoegen. We moeten nog wel zeggen wat de click gaat doen:
-
+openButton.addEventListener('click', openMenu) // Zo ga je een event (actie) toevoegen. We moeten nog wel zeggen wat de click gaat doen:
 
 // stap 3: voeg in de functie een class toe aan de nav
 function openMenu (){
-    NavSearch.classList.toggle('toonMenu');
+    NavSearch.classList.toggle('toonMenu')
 }
 
-// sluitButton.addEventListener('click', sluitMenu);
+sluitButton.addEventListener('click', sluitMenu)
 
 // stap 4 - in de functie verwijder de class van de nav
 function sluitMenu() {
-    NavSearch.classList.remove('toonMenu');
+    NavSearch.classList.remove('toonMenu')
 }
 
 
-//Stap 1: selecteren geschikt element
+
 //MARK: SEARCH
-const openSearchButton = document.querySelector('.search-button');
-const theSearch = document.querySelector('.zoeken');
-console.log(theSearch);
-openSearchButton.addEventListener('click', openSearch);
+//Stap 1: selecteren geschikt element
+const openSearchButton = document.querySelector('.search-button')
+const theSearch = document.querySelector('.zoeken')
+const closeSearchMenu = document.querySelector('.back--keyword-button')
 
 //Stap 2: voeg event en functie toe
+openSearchButton.addEventListener('click', openSearch)
+closeSearchMenu.addEventListener('click', closeSearch)
+
 function openSearch(){
-    theSearch.classList.toggle('toonSearch');
+    theSearch.classList.add('toonSearch')
+}
+
+function closeSearch() {
+    theSearch.classList.remove('toonSearch') //voor het verwijderen van de search balk zodra je op 'terug' klikt
 }
 
 //Stap 3: feedback aan de gebruiker --> CSS
